410587a2b097844a4169cf23dce3241de5ca9437
Iain <iain@polevaultweb.com>
2018-06-22 11:52:53 +0100

    Add filter for whitelisted user check, so the logic can be controlled with more flexibility than the constant allows. Fixes https://github.com/deliciousbrains/wp-migrate-db-anonymization/issues/14

--- includes/Config/Constraint.php
+++ includes/Config/Constraint.php
@@ -5,16 +5,21 @@ namespace WPMDB\Anonymization\Config;
 class Constraint {
 
 	public static function is_not_whitelisted_user( $row ) {
-		if ( ! defined( 'WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST' ) || empty( WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST ) ) {
-			return true;
-		}
-
-		$whitelisted_user_logins = explode( ',', WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST );
-
 		if ( ! isset( $row->user_login ) ) {
 			return true;
 		}
 
-		return ! in_array( $row->user_login, $whitelisted_user_logins );
+		return ! self::is_whitelisted_user( $row->user_login );
+	}
+
+	protected static function is_whitelisted_user( $user_login ) {
+		$whitelisted = false;
+		if ( defined( 'WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST' ) ) {
+			$whitelisted_user_logins = explode( ',', WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST );
+
+			$whitelisted = in_array( $user_login, $whitelisted_user_logins );
+		}
+
+		return apply_filters( 'wpmdb_anonymization_user_whitelisted', $whitelisted, $user_login );
 	}
 }
