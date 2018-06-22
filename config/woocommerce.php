5508a6c782b9efc8118562b5606b64bc3cec7d50
Iain <iain@polevaultweb.com>
2018-06-22 12:47:58 +0100

    Update WooCommerce config for missing meta and order post meta.

--- config/woocommerce.php
+++ config/woocommerce.php
@@ -86,6 +86,126 @@ return array(
 				'constraint'     => array( 'meta_key' => '_stripe_customer_id' ),
 				'fake_data_type' => 'ean13',
 			),
+			array(
+				'constraint'     => array( 'meta_key' => '_customer_ip_address' ),
+				'fake_data_type' => 'ipv4',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => 'Payer PayPal address' ),
+				'fake_data_type' => 'email',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => 'Payer first name' ),
+				'fake_data_type' => 'firstName',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => 'Payer last name' ),
+				'fake_data_type' => 'lastName',
+			),
+		),
+	),
+	'postmeta' => array(
+		'meta_value' => array(
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_first_name' ),
+				'fake_data_type' => 'firstName',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_last_name' ),
+				'fake_data_type' => 'lastName',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_company' ),
+				'fake_data_type' => 'company',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_address_1' ),
+				'fake_data_type' => 'streetAddress',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_address_2' ),
+				'fake_data_type' => 'secondaryAddress',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_city' ),
+				'fake_data_type' => 'city',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_postcode' ),
+				'fake_data_type' => 'postcode',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_state' ),
+				'fake_data_type' => 'state',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_country' ),
+				'fake_data_type' => 'countryCode',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_email' ),
+				'fake_data_type' => 'email',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_billing_phone' ),
+				'fake_data_type' => 'phoneNumber',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_first_name' ),
+				'fake_data_type' => 'firstName',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_last_name' ),
+				'fake_data_type' => 'lastName',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_company' ),
+				'fake_data_type' => 'company',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_address_1' ),
+				'fake_data_type' => 'streetAddress',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_address_2' ),
+				'fake_data_type' => 'secondaryAddress',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_city' ),
+				'fake_data_type' => 'city',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_postcode' ),
+				'fake_data_type' => 'postcode',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_state' ),
+				'fake_data_type' => 'state',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_shipping_country' ),
+				'fake_data_type' => 'countryCode',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_stripe_customer_id' ),
+				'fake_data_type' => 'ean13',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => '_customer_ip_address' ),
+				'fake_data_type' => 'ipv4',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => 'Payer PayPal address' ),
+				'fake_data_type' => 'email',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => 'Payer first name' ),
+				'fake_data_type' => 'firstName',
+			),
+			array(
+				'constraint'     => array( 'meta_key' => 'Payer last name' ),
+				'fake_data_type' => 'lastName',
+			),
 		),
 	),
 );
