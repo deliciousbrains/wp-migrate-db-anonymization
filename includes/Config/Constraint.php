9ce03bf1a2caa82fed51841c1d43ff0037886d2f
Iain <iain@polevaultweb.com>
2018-09-11 14:47:37 +0100

    Convert the row of data to a WP_User instance and pass that to the user whitelist filter for more flexibility. Fixes https://github.com/deliciousbrains/wp-migrate-db-anonymization/issues/21

--- includes/Config/Constraint.php
+++ includes/Config/Constraint.php
@@ -9,17 +9,24 @@ class Constraint {
 			return true;
 		}
 
-		return ! self::is_whitelisted_user( $row->user_login );
+		$user = new \WP_User( $row );
+
+		return ! self::is_whitelisted_user( $user );
 	}
 
-	protected static function is_whitelisted_user( $user_login ) {
+	/**
+	 * @param \WP_User $user
+	 *
+	 * @return bool
+	 */
+	protected static function is_whitelisted_user( $user ) {
 		$whitelisted = false;
 		if ( defined( 'WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST' ) ) {
 			$whitelisted_user_logins = explode( ',', WPMDB_ANONYMIZATION_USER_LOGIN_WHITELIST );
 
-			$whitelisted = in_array( $user_login, $whitelisted_user_logins );
+			$whitelisted = in_array( $user->user_login, $whitelisted_user_logins );
 		}
 
-		return apply_filters( 'wpmdb_anonymization_user_whitelisted', $whitelisted, $user_login );
+		return (bool) apply_filters( 'wpmdb_anonymization_user_whitelisted', $whitelisted, $user );
 	}
 }
